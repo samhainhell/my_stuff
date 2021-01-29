0c04a52fef619a8b1e0181638c0ef995f73e1876
samhainhell <78235766+samhainhell@users.noreply.github.com>
2021-01-29 21:34:08 +0000

    Update toruk.lua

--- toruk.lua
+++ toruk.lua
@@ -1,3 +1,5 @@
+-- Script that kills a tribe when all its units die, yet the Shaman is still inside a prison
+
 import(Module_System)
 import(Module_Players)
 import(Module_Defines)
@@ -11,54 +13,35 @@ include("UtilPThings.lua")
 include("UtilRefs.lua")
 
 for i = 0, 7 do
-
 	SET_NO_REINC(i)
-
 end
 
 function OnTurn()
-
+	
 	if everyPow(24, 1) then
-		
 		if _gsi.Counts.GameTurn > 240 then
-
 			local count = 0
-
 			for i = 0, 7 do
-
 				local tribe = _gsi.Players[i].NumPeople
-
 				if tribe == 1 then
-
 					local shaman = getShaman(count)
 					if (shaman ~= nil) then
-
 						if shaman.State == S_PERSON_SHAMAN_IN_PRISON then
-
 							local mp = MapPosXZ.new()
 							mp.Pos = world_coord2d_to_map_idx(shaman.Pos.D2)
 							KILL_TEAM_IN_AREA(mp.XZ.X, mp.XZ.Z, 0)
 							count = count + 1
-
 						else
-
 							count = count + 1
-
 						end
-
 					else
-
 						count = count + 1
-
 					end
-
 				else
-
 					count = count + 1
-
 				end
 			end
 		end
 	end
-
+	
 end
